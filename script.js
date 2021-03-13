b4af5e72549f9b649087bd9764340d182a9f3090
mrdietz <mrdietz@gmail.com>
2021-03-13 13:03:07 -0800

    Dom manipulation fixed

--- script.js
+++ script.js
@@ -6,51 +6,41 @@ const playerChoice = document.querySelectorAll(".btn");
 playerChoice.forEach((button) => button.addEventListener("click", game));
 
 function game(e) {
+
     function computerPlay() {
         const compChoice = ["paper", "rock", "scissors"];  
         const random = Math.floor(Math.random() * compChoice.length);
         return compChoice[random];  
-    }
+    };
 
     let playerSelection = e.target.value;
-    const computerSelection = computerPlay();
+    let computerSelection = computerPlay();
+    let result;
 
-    let result = (function playRound(playerSelection, computerSelection) {
-        if (playerSelection === computerSelection) {
-        return "tie";
-        
-        } else if ((playerSelection === "scissors" && computerSelection === "paper") || (playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock")) {
-            playerScore ++; 
-            return "pWin";
+    if (playerSelection == computerSelection) {
+    result = "tie";
+    document.getElementById("message").innerHTML = "Tie, you're both losers!";
 
-        } else if ((playerSelection === "paper" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "rock") || (playerSelection === "rock" && computerSelection === "paper")) {
-            computerScore ++;
-            return "cWin";
-        }
-    })();
-document.getElementById("pScore").innerHTML = playerScore;
-    if (result === "tie") {
-        document.getElementById("pChoice").innerHTML = playerSelection;
-        document.getElementById("cChoice").innerHTML = computerSelection;
-        document.getElementById("message").innerHTML = "Tie, you're both losers!";
-
-    } else if (result === "pWin") {
-        document.getElementById("pChoice").innerHTML = playerSelection;
-        document.getElementById("cChoice").innerHTML = computerSelection;
+    } else if ((playerSelection == "scissors" && computerSelection == "paper") || (playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "paper" && computerSelection == "rock")) {
+        result = "pWin";
+        playerScore ++; 
         document.getElementById("message").innerHTML = "Despite what I've heard, you're a real winner!";
 
-    } else if (result === "cWin") {
-        document.getElementById("pChoice").innerHTML = playerSelection;
-        document.getElementById("cChoice").innerHTML = computerSelection;
+    } else if ((playerSelection == "paper" && computerSelection == "scissors") || (playerSelection == "scissors" && computerSelection == "rock") || (playerSelection == "rock" && computerSelection == "paper")) {
+        result = "cWin";
+        computerScore ++;
         document.getElementById("message").innerHTML = "You disappoint me...again.";
-    } 
+    }
 
-    // console.log("You chose " + playerSelection + ".");
-    // console.log("Computer chose " + computerSelection + ".")
-    // console.log(playRound(playerSelection, computerSelection));
-}
-        
-// var i;
-// for (i = 0; i < 5; i++) {
-//     game ();
-// }
+    document.getElementById("pScore").innerHTML = playerScore;
+    document.getElementById("cScore").innerHTML = computerScore;
+    document.getElementById("pChoice").innerHTML = playerSelection;
+    document.getElementById("cChoice").innerHTML = computerSelection;
+
+    if ((computerScore == 5) || (playerScore == 5)) {
+        document.getElementById("gameover").innerHTML = "GAME OVER!";
+        document.getElementsByClassName("btn")[0].disabled = true;
+        document.getElementsByClassName("btn")[1].disabled = true;
+        document.getElementsByClassName("btn")[2].disabled = true;
+    }
+};
